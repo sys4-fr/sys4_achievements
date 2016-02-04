ea6699dfa247892e94ccf9749c0bb35c6f26d9fe
sys4-fr <sys4@free.fr>
2016-02-04 17:47:47 +0100

    Reorganize achievements definition part. 2 (not finished yet)

---
+++ achievements/crafter.lua
@@ -0,0 +1,75 @@
+local a = sys4_achievements.achievements['sys4']
+local mkach = sys4_achievements.mkach
+
+local t = 'craft' -- Type of achievement
+
+-- Requieres 'papyrus_digger' --
+
+-- book_crafter
+table.insert(a, mkach(t, "book_crafter", "You know write ?", "default_book.png", "default:paper", 3, nil, {'default:book', 'default:bookshelf'}, nil, "papyrus_digger"))
+
+-- Requieres 'book_crafter' --
+
+-- bookshelf_crafter
+table.insert(a, mkach(t, "bookshelf_crafter", "Get the knowledge", "default_bookshelf.png", "default:bookshelf", 1, nil, nil, nil, "book_crafter"))
+
+-- Requieres 'tree_digger' --
+
+-- planks_crafter
+table.insert(a, mkach(t, "planks_crafter", "Make planks", "default_wood.png", "default:wood", 8, nil, {'default:stick', 'default:chest', 'doors:door_wood', 'doors:trapdoor', 'boats:boat'}, "Make planks from wood", "tree_digger"))
+
+-- Requieres 'planks_crafter" --
+
+-- sticks_crafter
+table.insert(a, mkach(t, "sticks_crafter", "Make sticks", "default_stick.png", "default:stick", 8, nil, {'default:sword_wood', 'default:axe_wood', 'default:shovel_wood', 'farming:hoe_wood', 'default:ladder', 'default:fence_wood'}, "Make sticks from planks", "planks_crafter"))
+
+-- Requieres 'sticks_crafter' --
+
+-- axe_crafter
+table.insert(a, mkach(t, "axe_crafter", "Make a wooden axe", "default_tool_woodaxe.png", "default:axe_wood", 1, nil, nil, "Make a wooden axe from sticks and planks", "sticks_crafter"))
+
+-- sword_crafter
+table.insert(a, mkach(t, "sword_crafter", "Make a wooden sword", "default_tool_woodsword.png", "default:sword_wood", 1, nil, nil, "Make a wooden sword from sticks and planks", "sticks_crafter"))
+
+-- shovel_crafter
+table.insert(a, mkach(t, "shovel_crafter", "Make a wooden shovel", "default_tool_woodshovel.png", "default:shovel_wood", 1, nil, nil, "Make a wooden shovel from sticks and planks", "sticks_crafter"))
+
+-- hoe_crafter
+table.insert(a, mkach(t, "hoe_crafter", "Make a wooden hoe", "farming_tool_woodhoe.png", "farming:hoe_wood", 1, nil, nil, "Make a wooden hoe from sticks and planks", "sticks_crafter"))
+
+-- Requieres 'award_lumberjack' --
+
+-- pick_crafter
+table.insert(a, mkach(t, "pick_crafter", "Make a wooden pickaxe", "default_tool_woodpick.png", "default:pick_wood", 1, nil, nil, "Make a wooden pick from sticks and planks", "award_lumberjack"))
+
+-- Requieres 'coal_digger' --
+
+-- dye_crafter
+table.insert(a, mkach(t, "dye_crafter", "A colorful World", "dye_red.png", "dye:black", 100, nil, {'dye:red', 'dye:blue', 'dye:yellow', 'dye:white', 'dye:orange', 'dye:violet', 'dye:green'}, "Discover how to make others colored dyes", "coal_digger"))
+
+-- dye_crafter_lover
+table.insert(a, mkach(t, "dye_crafter_lover", "Picasso", "dye_brown.png", "dye:black", 200, nil, {'dye:brown','dye:cyan', 'dye:dark_green', 'dye:dark_grey', 'dye:grey', 'dye:magenta', 'dye:pink'}, nil, "coal_digger"))
+
+-- wool_crafter
+table.insert(a, mkach(t, "wool_crafter", "Colored Wools", "wool_red.png", "wool:black", 100, nil, {'wool:red', 'beds:bed_bottom', 'beds:fancy_bed_bottom', 'wool:blue', 'wool:yellow', 'wool:orange', 'wool:violet', 'wool:green'}, "Discover how to make colored wools", "coal_digger"))
+
+-- wool_crafter_lover
+table.insert(a, mkach(t, "wool_crafter_lover", "The Rainbow Wools", "wool_brown.png", "wool:red", 200, nil, {'wool:brown','wool:cyan', 'wool:dark_green', 'wool:dark_grey', 'wool:grey', 'wool:magenta', 'wool:pink'}, nil, "coal_digger"))
+
+-- Requieres 'wool_crafter' --
+
+-- bed_crafter
+table.insert(a, mkach(t, "bed_crafter", "Going to sleep", "beds_bed.png", "beds:bed_bottom", 1, nil, nil, "Make a bed from planks and wools", "wool_crafter"))
+
+-- Requires 'award_mine2' --
+
+-- pick_crafter_lover
+table.insert(a, mkach(t, "pick_crafter_lover", "Better Pick!", "default_tool_stonepick.png", "default:pick_stone", 1, nil, nil, "Make a better quality Pick from sticks and stones", "award_mine2"))
+
+-- Requires 'copper_digger' --
+
+-- bronze_crafter
+table.insert(a, mkach(t, "bronze_crafter", "The Bronze Age", "default_bronze_ingot.png", "default:bronze_ingot", 1, nil, {'default:sword_bronze', 'default:axe_bronze', 'default:shovel_bronze', 'farming:hoe_bronze'}, nil, "copper_digger"))
+
+-- bronze_crafter_lover
+table.insert(a, mkach(t, "bronze_crafter_lover", "The Bronze Peak", "default_bronze_ingot.png", "default:bronze_ingot", 100, nil, {'default:pick_bronze', 'default:bronze_block'}, nil, "copper_digger"))
--- achievements/digger.lua
+++ achievements/digger.lua
@@ -1,24 +1,60 @@
+local S = sys4_achievements.intllib
+
 local a = sys4_achievements.achievements['sys4']
 local mkach = sys4_achievements.mkach
 
 local t = 'dig' -- Type of achievement
 
-table.insert(a, mkach(t, "tree_digger", "Finding wood", "default_tree.png", "default:tree", 1, nil,
-		      {'default:wood', 'default:junglewood', 'default:pine_wood', 'default:acacia_wood'},
-		      "Attack a tree and obtain wood. It's an essential raw material",
-		      nil))
+local awardTmp, titems
 
--- Dig 13 wheats : unlock flour & straws
-table.insert(a, mkach(t, "wheat_digger", "Hands in the Flour", "farming_flour.png", "farming:wheat_8", 8,
-		      nil, {'farming:flour', 'farming_straw', 'farming_wheat'},
-		      nil, "hoe_crafter"))
--- Cotton
-table.insert(a, mkach(t, "cotton_digger", "Spinner's first steps", "wool_white.png", "farming:cotton_8", 8,
-		      nil, {'wool:white'}, nil, "hoe_crafter"))
+-- Requires Nothing ! --
 
--- Stone
-table.insert(a, mkach(t, "stone_digger", "Finding stone", "default_stone.png", "default:stone", 1, nil,
-		      {'default:furnace', 'default:axe_stone', 'default:sword_stone', 'default:shovel_stone', 'farming:hoe_stone'},
-		      "Find stone and make your first stone tools", "pick_crafter"))
+-- papyrus_digger
+table.insert(a, mkach(t, "papyrus_digger", "Your papers please", "default_paper.png", "default:papyrus", 3, nil, {'default:paper'}, nil, nil))
 
--- Coal
+-- tree_digger
+table.insert(a, mkach(t, "tree_digger", "Finding wood", "default_tree.png", "default:tree", 1, nil, {'default:wood', 'default:junglewood', 'default:pine_wood', 'default:acacia_wood'}, "Attack a tree and obtain wood. It's an essential raw material", nil))
+
+-- award_lumberjack
+awardTmp = awards.def['award_lumberjack']
+titems = {'default:pick_wood'}
+awardTmp.items = titems
+awardTmp.book = {title = "SYS4 AWARDS : "..awardTmp.title,
+		 text = sys4_achievements.write_book( S("Discover how to make a wooden pickaxe."), titems, awardTmp.prizes)
+}
+
+-- Requires 'hoe_crafter' --
+
+-- wheat_digger
+table.insert(a, mkach(t, "wheat_digger", "Hands in the Flour", "farming_flour.png", "farming:wheat_8", 8, nil, {'farming:flour', 'farming_straw', 'farming_wheat'}, nil, "hoe_crafter"))
+
+-- cotton_digger
+table.insert(a, mkach(t, "cotton_digger", "Spinner's first steps", "wool_white.png", "farming:cotton_8", 8, nil, {'wool:white'}, nil, "hoe_crafter"))
+
+-- Require 'pick_crafter' --
+
+-- award_mine2
+awardTmp = awards.def['award_mine2']
+titems = {'default:pick_stone'}
+awardTmp.award_req = "pick_crafter"
+awardTmp.items = titems
+awardTmp.book = {title = "SYS4 AWARDS : "..awardTmp.title,
+		 text = sys4_achievements.write_book( S("Discover how to make a stone pickaxe."), titems, awardTmp.prizes)
+}
+
+-- award_youre_winner'
+awards.def['award_youre_winner'].award_req = 'pick_crafter'
+      
+-- stone_digger
+table.insert(a, mkach(t, "stone_digger", "Finding stone", "default_stone.png", "default:stone", 1, nil, {'default:furnace', 'default:axe_stone', 'default:sword_stone', 'default:shovel_stone', 'farming:hoe_stone'}, "Find stone and make your first stone tools", "pick_crafter"))
+
+-- coal_digger
+table.insert(a, mkach(t, "coal_digger", "Finding coal", "default_coal_lump.png", "default:stone_with_coal", 1, nil, {'default:torch', 'dye:black', 'wool:black', 'default:coalblock', 'default:coal'}, "Find coal and discover his usages", "pick_crafter"))
+
+-- Require 'pick_crafter_lover' --
+
+-- iron_digger
+table.insert(a, mkach(t, "iron_digger", "The Iron Age", "default_steel_ingot.png", "default:stone_with_iron", 10, nil, {'bucket:bucket_empty', 'xpanes:bar', 'doors:door_steel', 'vessels:steel_bottle', 'screwdriver:screwdriver', 'default:sword_steel', 'default:axe_steel', 'default:shovel_steel', 'farming:hoe_steel'}, nil, "pick_crafter_lover"))
+
+-- copper_digger
+table.insert(a, mkach(t, "copper_digger", "The Copper Age", "default_copper_ingot.png", "default:stone_with_copper", 100, nil, {'default:copperblock', 'default:copper_ingot', 'default:bronze_ingot'}, nil, "pick_crafter_lover"))
